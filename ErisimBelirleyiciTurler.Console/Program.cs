ef81a88c5be8e0d829d540171ea558c7136ae019
Bora Saglam <posarobit@gmail.com>
2025-08-15 10:54:34 +0300

    Add project files.

---
+++ ErisimBelirleyiciTurler.Console/Program.cs
@@ -0,0 +1,81 @@
+﻿// Musteri.cs dosyası
+
+
+public class Musteri
+{
+    public string AdiSoyadi { get; set; }
+    public void BilgileriYazdir()
+    {
+        Console.WriteLine($"Müşterinin Adı: {AdiSoyadi}");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Musteri musteri1 = new Musteri();
+        musteri1.AdiSoyadi = "Malik Sarkan";
+    }
+}
+
+public class BankaHesabi
+{
+    private decimal _bakiye;
+
+    public BankaHesabi(decimal baslangicBakiye)
+    {
+        _bakiye = baslangicBakiye;
+    }
+
+
+
+    public void ParaYatir(decimal miktar)
+    {
+        if (miktar > 10)
+        {
+            _bakiye += miktar;
+            GunlukLimitiKontrolEt();
+        }
+    }
+
+    private void GunlukLimitiKontrolEt()
+    {
+        throw new NotImplementedException();
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            BankaHesabi hesap = new BankaHesabi(1500);
+            hesap.ParaYatir(430);
+        }
+    }
+
+    internal record struct NewStruct(object Item1, object Item2)
+    {
+        public static implicit operator (object, object)(NewStruct value)
+        {
+            return (value.Item1, value.Item2);
+        }
+
+        public static implicit operator NewStruct((object, object) value)
+        {
+            return new NewStruct(value.Item1, value.Item2);
+        }
+    }
+}
+
+internal record struct NewStruct1(object Item1, object Item2)
+{
+    public static implicit operator (object, object)(NewStruct1 value)
+    {
+        return (value.Item1, value.Item2);
+    }
+
+    public static implicit operator NewStruct1((object, object) value)
+    {
+        return new NewStruct1(value.Item1, value.Item2);
+    }
+}
