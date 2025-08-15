20b40a3b9b3fe254710e601624e6d1170b3ccfe2
Bora Saglam <posarobit@gmail.com>
2025-08-15 12:20:04 +0300

    Yeni repo.

--- ErisimBelirleyiciTurler.Console/Program.cs
+++ ErisimBelirleyiciTurler.Console/Program.cs
@@ -15,7 +15,7 @@ class Program
     static void Main()
     {
         Musteri musteri1 = new Musteri();
-        musteri1.AdiSoyadi = "Malik Burak Sarkan ";
+        musteri1.AdiSoyadi = "Malik Burak Sarkan";
     }
 }
 
